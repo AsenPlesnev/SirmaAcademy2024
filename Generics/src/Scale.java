9601ecbb04702fbc7a94b71fd972f2a4f55c71ad
Asen Plesnev <as.plesnev@gmail.com>
2024-07-04 15:57:12 +0300

    Added Generic Scale Task

---
+++ Generics/src/Scale.java
@@ -0,0 +1,35 @@
+public class Scale<T extends Comparable<T>> {
+    private T left;
+    private T right;
+
+    public Scale(T left, T right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public T getLeft() {
+        return left;
+    }
+
+    public void setLeft(T left) {
+        this.left = left;
+    }
+
+    public T getRight() {
+        return right;
+    }
+
+    public void setRight(T right) {
+        this.right = right;
+    }
+
+    public T getHeavier() {
+        if(this.left.compareTo(this.right) == 0) {
+            return null;
+        } else if (this.left.compareTo(this.right) < 0) {
+            return right;
+        }
+
+        return left;
+    }
+}
