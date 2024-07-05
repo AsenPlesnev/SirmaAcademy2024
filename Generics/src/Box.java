b3948545a06ecc77073fe9d90eba2c03f4c7a586
Asen Plesnev <as.plesnev@gmail.com>
2024-07-05 11:50:08 -0500

    Added Generic method that counts number of elements that are bigger then given element

--- Generics/src/Box.java
+++ Generics/src/Box.java
@@ -1,15 +1,21 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class Box<T> {
-    private T item;
+public class Box<T extends Comparable<T>> implements Comparable<Box<T>> {
+    private final T item;
 
     public Box(T item) {
         this.item = item;
     }
 
+    public T getItem() {
+        return this.item;
+    }
+
     @Override
     public String toString() {
         return this.item.getClass().getName() + (": ") + this.item;
+    }
+
+    @Override
+    public int compareTo(Box<T> o) {
+        return this.item.compareTo(o.item);
     }
 }
--- Generics/src/Main.java
+++ Generics/src/Main.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       
+
     }
 
     public static <T> void swapElementsOfList(List<T> list, int index1, int index2) {
@@ -14,5 +15,16 @@ public class Main {
         T temp = list.get(index2);
         list.set(index2, list.get(index1));
         list.set(index1, temp);
+    }
+
+    public static <T extends  Comparable<T>> int countGreaterThan(List<T> list, T element) {
+        int count = 0;
+        for (T item : list) {
+            if (item.compareTo(element) > 0) {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
